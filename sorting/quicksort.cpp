8516f80da0ec9d8b83aae2a74866568eedf5d304
Anas khan <reyaz7753@gmail.com>
2026-02-17 23:40:32 +0530

    update file with full code

--- sorting/quicksort.cpp
+++ sorting/quicksort.cpp
@@ -1,21 +1,47 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
+int partion(vector<int> &arr, int low, int high)
+{
 
+    int pivot = arr[high];
+    int i = low - 1;
 
-
-
-int partition( vector<int> &arr , int start , int end){
-if(int start<end){
-    int pvtindx = quicksort(arr,start,end);
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i+1;
 }
+
+void quicksort(vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partion(arr, low, high);
+        quicksort(arr, low, pi-1);
+        quicksort(arr, pi + 1, high);
+    }
 }
 
-int main(){
-    vector<int> arr = {5,7,2,4,6};
-    int end = arr.size()-1;
-    int start = arr[0]
+int main()
+{
+    vector<int> arr = {10, 7, 8, 9, 1, 5};
+    int n = arr.size();
 
-  
+    quicksort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
